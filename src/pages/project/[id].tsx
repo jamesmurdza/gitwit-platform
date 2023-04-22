a5acbe6421bf41b59f921b5d2fc5074522723055
James Murdza <james@jamesmurdza.com>
2023-04-22 10:27:53 +0200

    Add progress indicators and GitHub links to the project details page.

--- src/pages/project/[id].tsx
+++ src/pages/project/[id].tsx
@@ -1,17 +1,22 @@
 import { Suspense, useState } from "react"
 import { useQuery } from "@blitzjs/rpc"
 import Layout from "src/layouts/layout"
+import { BarLoader } from "react-spinners"
 import {
   CodeBracketSquareIcon,
   ClockIcon,
   ExclamationTriangleIcon,
   DocumentIcon,
+  FolderIcon,
 } from "@heroicons/react/24/outline"
 import { Fragment } from "react"
 import { Menu, Transition } from "@headlessui/react"
 import { EllipsisVerticalIcon } from "@heroicons/react/20/solid"
 import getProject from "src/projects/queries/getProject"
 import { useParam } from "@blitzjs/next"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCodeBranch } from "@fortawesome/free-solid-svg-icons"
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
 
 const secondaryNavigation = [
   { name: "Code", href: "#", icon: CodeBracketSquareIcon, current: true },
@@ -51,6 +56,14 @@ export function ProjectView() {
   const id = typeof idParam == "string" ? Number.parseInt(idParam) : undefined
   const [project, isLoading] = useQuery(getProject, { id }, { refetchInterval: 5000 })
 
+  const getRegexMatch = (regex: RegExp, string: string) => {
+    const matches: RegExpExecArray = regex.exec(string)
+    return matches ? matches[1] : ""
+  }
+
+  const fullRepositoryName = getRegexMatch(/github\.com\/(.+)(\.git)?/, project.repositoryURL || "")
+  const htmlRepositoryURL = getRegexMatch(/(.+)(\.git)?/, project.repositoryURL || "")
+
   return (
     <>
       <div className="mx-auto max-w-7xl px-4 py-4 sm:px-6 lg:px-8">
@@ -58,39 +71,36 @@ export function ProjectView() {
           <div className="absolute inset-0 -z-10 overflow-hidden" aria-hidden="true">
             <div className="absolute inset-x-0 bottom-0 h-px bg-gray-900/5" />
           </div>
-          <div className="mx-auto max-w-7xl px-4 py-10 sm:px-6 lg:px-8">
+          <div className="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
             <div className="mx-auto flex max-w-2xl items-center justify-between gap-x-8 lg:mx-0 lg:max-w-none">
-              <div className="flex items-center gap-x-6">
-                <img
-                  src="https://tailwindui.com/img/logos/48x48/tuple.svg"
-                  alt=""
-                  className="h-16 w-16 flex-none rounded-full ring-1 ring-gray-900/10"
-                />
+              <div className="flex flex-wrap items-center gap-x-6">
                 <h1>
-                  <div className="mt-1 text-base font-semibold leading-6 text-gray-900">
+                  <div className="leading-6 text-gray-900 text-2xl">
+                    <FolderIcon className="inline-block align-text-bottom h-5 mb-0.5 mr-2" />
                     {project.repositoryName}
                   </div>
-                  <div className="text-sm leading-6 text-gray-500">{project.description}</div>
                 </h1>
+                {project.repositoryURL && (
+                  <div className="text-sm leading-6 text-gray-500 mt-1">
+                    <a href={htmlRepositoryURL} target="_blank" rel="noreferrer">
+                      <FontAwesomeIcon icon={faGithub} /> {fullRepositoryName}
+                      <FontAwesomeIcon icon={faCodeBranch} className="ml-4" /> main
+                    </a>
+                  </div>
+                )}
               </div>
               <div className="flex items-center gap-x-4 sm:gap-x-6">
                 <button
                   type="button"
                   className="hidden text-sm font-semibold leading-6 text-gray-900 sm:block"
                 >
-                  Copy URL
+                  View code
                 </button>
-                <a
-                  href="#"
-                  className="hidden text-sm font-semibold leading-6 text-gray-900 sm:block"
-                >
-                  Edit
-                </a>
                 <a
                   href="#"
                   className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                  Send
+                  Deploy
                 </a>
 
                 <Menu as="div" className="relative sm:hidden">
@@ -118,21 +128,8 @@ export function ProjectView() {
                               "block w-full px-3 py-1 text-left text-sm leading-6 text-gray-900"
                             )}
                           >
-                            Copy URL
+                            View on GitHub
                           </button>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? "bg-gray-50" : "",
-                              "block px-3 py-1 text-sm leading-6 text-gray-900"
-                            )}
-                          >
-                            Edit
-                          </a>
                         )}
                       </Menu.Item>
                     </Menu.Items>
@@ -143,7 +140,7 @@ export function ProjectView() {
           </div>
         </header>
         <div className="mx-auto max-w-7xl lg:flex lg:gap-x-16 lg:px-8">
-          <aside className="flex overflow-x-auto border-b border-gray-900/5 py-4 lg:block lg:w-64 lg:flex-none lg:border-0 lg:py-12">
+          <aside className="flex overflow-x-auto border-b border-gray-900/5 py-4 lg:block lg:w-48 lg:flex-none lg:border-0 lg:py-12">
             <nav className="flex-none px-4 sm:px-6 lg:px-0">
               <ul role="list" className="flex gap-x-3 gap-y-1 whitespace-nowrap lg:flex-col">
                 {secondaryNavigation.map((item) => (
@@ -175,36 +172,30 @@ export function ProjectView() {
           </aside>
           <main className="px-4 py-16 sm:px-6 lg:flex-auto lg:px-0 lg:py-12">
             {(project.build == undefined || project.build.status == "RUNNING") && (
-              <div className="bg-gray-50 sm:rounded-lg mb-12">
+              <div className="bg-gray-50 sm:rounded-lg mb-12 text-center">
                 <div className="px-4 py-5 sm:p-6">
                   <h3 className="text-base font-semibold leading-6 text-gray-900">
                     {project.build ? "Build in progress" : "New project"}
                   </h3>
-                  <div className="mt-2 max-w-xl text-sm text-gray-500">
+                  <div className="mt-2 text-sm text-gray-500">
                     <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
-                      praesentium tenetur pariatur.
+                      Your repository is being generated. This should take less than two minutes.
                     </p>
                   </div>
-                  <div className="mt-5">
-                    <button
-                      type="button"
-                      className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                    >
-                      Contact sales
-                    </button>
+                  <div className="mt-5 flex justify-center">
+                    <BarLoader />
                   </div>
                 </div>
               </div>
             )}
             {project.build && project.build.status == "SUCCESS" && (
               <>
-                <div className="bg-gray-50 sm:rounded-lg mb-12">
+                <div className="bg-gray-50 sm:rounded-lg mb-12 text-center">
                   <div className="px-4 py-5 sm:p-6">
                     <h3 className="text-base font-semibold leading-6 text-gray-900">
-                      Need more bandwidth?
+                      Add features or fix bugs
                     </h3>
-                    <div className="mt-2 max-w-xl text-sm text-gray-500">
+                    <div className="mt-2 text-sm text-gray-500">
                       <p>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
                         praesentium tenetur pariatur.
@@ -215,7 +206,7 @@ export function ProjectView() {
                         type="button"
                         className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
                       >
-                        Contact sales
+                        Make a new version
                       </button>
                     </div>
                   </div>
