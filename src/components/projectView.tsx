68c3e98a135f15b891b8239aaf90cde601e078f9
James Murdza <james@jamesmurdza.com>
2023-06-11 13:56:54 -0400

    Change the deploy button to a dropdown menu.

--- src/components/projectView.tsx
+++ src/components/projectView.tsx
@@ -2,7 +2,7 @@ import { Suspense, Fragment } from "react"
 import Head from "next/head"
 
 import { Menu, Transition } from "@headlessui/react"
-import { EllipsisVerticalIcon } from "@heroicons/react/20/solid"
+import { EllipsisVerticalIcon, ChevronDownIcon } from "@heroicons/react/20/solid"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faCodeBranch } from "@fortawesome/free-solid-svg-icons"
 import { faGithub } from "@fortawesome/free-brands-svg-icons"
@@ -11,10 +11,54 @@ import { FolderIcon } from "@heroicons/react/24/outline"
 // This causes the Font Awesome icons to size down properly:
 import "@fortawesome/fontawesome-svg-core/styles.css"
 
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ")
+}
+
+function LaunchButton({ launchURL }) {
+  return (
+    <Menu as="div" className="relative inline-block text-left">
+      <div>
+        <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+          Launch
+          <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
+        </Menu.Button>
+      </div>
+
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="absolute right-0 z-10 mt-2 w-32 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <div className="py-1">
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href={launchURL}
+                  target="_blank"
+                  className={classNames(
+                    active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                    "block px-4 py-2 text-sm"
+                  )}
+                  rel="noreferrer"
+                >
+                  Codespace
+                </a>
+              )}
+            </Menu.Item>
+          </div>
+        </Menu.Items>
+      </Transition>
+    </Menu>
+  )
+}
+
 function ProjectHeader({ project }) {
-  function classNames(...classes) {
-    return classes.filter(Boolean).join(" ")
-  }
   const htmlRepositoryURL = project.build?.outputHTMLURL
   const regex = /\/\/github\.com\/([\w-]+)\/([\w-]+)(\/tree\/([\w-]+))?/
   const [, repositoryUsername, repositoryName, , branchNameComponent] =
@@ -60,14 +104,9 @@ function ProjectHeader({ project }) {
             >
               View code
             </a>
-            <a
-              href={`https://github.com/codespaces/new?repo=${repositoryUsername}/${repositoryName}&ref=${branchName}`}
-              target="_blank"
-              className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-              rel="noreferrer"
-            >
-              Deploy
-            </a>
+            <LaunchButton
+              launchURL={`https://github.com/codespaces/new?repo=${repositoryUsername}/${repositoryName}&ref=${branchName}`}
+            />
 
             <Menu as="div" className="relative sm:hidden">
               <Menu.Button className="-m-3 block p-3">
